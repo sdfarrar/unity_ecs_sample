a004ec22c45a3162d64f1954f71cde16cc425998
Seth Farrar <seth@ai-sys.com>
2018-05-18 13:20:50 -0500

    ECS setup

---
+++ Assets/Scripts/Bootstrap.cs
@@ -0,0 +1,27 @@
+﻿using Unity.Entities;
+using Unity.Rendering;
+using Unity.Transforms;
+using UnityEngine;
+
+public class Bootstrap : MonoBehaviour {
+
+	public Mesh PlayerMesh;
+	public Material PlayerMaterial;
+
+	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+	private void Start () {
+		EntityManager entityManager = World.Active.GetOrCreateManager<EntityManager>();
+		EntityArchetype playerArchetype = entityManager.CreateArchetype(
+			typeof(TransformMatrix),
+			typeof(Position),
+			typeof(MeshInstanceRenderer)
+		);
+
+		Entity player = entityManager.CreateEntity(playerArchetype);
+		entityManager.SetSharedComponentData(player, new MeshInstanceRenderer{
+			mesh = PlayerMesh,
+			material = PlayerMaterial
+		});
+	}
+
+}
